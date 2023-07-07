b67758746e506f372187aa1a952f94114490acac
Falderian <nikitastaselovich@gmail.com>
2023-07-07 13:56:01 +0300

    ADD: add solution to FS tasks

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,10 +1,15 @@
 import path from "path";
 import { readdir } from "fs/promises";
+import { existsSync } from "fs";
 
 const list = async () => {
   const folderToRead = path.resolve(process.cwd() + "/src/fs/files");
+  if (!existsSync(folderToRead))
+    throw new Error("Folder to read doesnt exists");
   try {
-    readdir(folderToRead, (err, files) => console.log(files || err));
+    readdir(folderToRead, (err, files) => files).then((files) =>
+      console.log(files)
+    );
   } catch (error) {
     console.log(error);
   }
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import path from "path";
+import { readFile } from "fs/promises";
+import { existsSync } from "fs";
+
 const read = async () => {
-    // Write your code here 
+  const fileToRead = path.resolve(
+    process.cwd() + "/src/fs/files/fileToRead.txt"
+  );
+
+  if (!existsSync(fileToRead)) throw new Error("There is no file to read.");
+
+  readFile(fileToRead, (err, data) => {
+    if (err) throw new Error("FS operation failed");
+    return data;
+  }).then((data) => console.log(data.toString()));
 };
 
 await read();
