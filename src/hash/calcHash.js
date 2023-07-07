caacc10a4a688e15399ba4b28c2ea71c37ab0475
Falderian <nikitastaselovich@gmail.com>
2023-07-07 15:11:08 +0300

    ADD: add solution to hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import path from "path";
+import { createHash } from "crypto";
+import { readFile } from "fs/promises";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const fileToCalc = path.resolve(
+    process.cwd() + "/src/hash/files/fileToCalculateHashFor.txt"
+  );
+
+  const file = await readFile(fileToCalc);
+  const hash = createHash("sha256");
+
+  console.log(hash.update(file).digest("hex"));
 };
 
 await calculateHash();
