4c6a82a4bc30f4879a80196f507a00f51e338b9d
Nikita Staselovich <nikitastaselovich@gmail.com>
2023-07-10 13:48:42 +0300

    ADD: add solution to streams task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,11 +1,15 @@
 import { createReadStream } from "fs";
+import { stdout } from "process";
 import path from "path";
-import { readFile } from "fs/promises";
 
 const read = async () => {
   const fileToRead = path.resolve(
     process.cwd() + "/src/streams/files/fileToRead.txt"
   );
+
+  createReadStream(fileToRead).on("data", (chunk) => {
+    stdout.write(chunk);
+  });
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import { createWriteStream } from "fs";
+import { stdin } from "process";
+import path from "path";
+
 const write = async () => {
-    // Write your code here 
+  const fileToWrite = path.resolve(
+    process.cwd() + "/src/streams/files/fileToWrite.txt"
+  );
+
+  const stream = createWriteStream(fileToWrite);
+
+  console.log("Type your text here:");
+
+  stdin.on("data", (data) => {
+    stream.write(data);
+  });
 };
 
 await write();
