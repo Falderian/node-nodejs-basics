81419e263e1bcae9920b2528005945be5c1f1f7c
Nikita Staselovich <nikitastaselovich@gmail.com>
2023-07-10 16:20:50 +0300

    ADD: add solution to workers task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,18 @@
+import { Worker, parentPort } from "worker_threads";
+import { cpus } from "os";
+import path from "path";
+
 const performCalculations = async () => {
-    // Write your code here
+  let increment = 10;
+
+  const workerFile = path.resolve(process.cwd() + "/src/wt/worker.js");
+
+  cpus().forEach(() => {
+    const worker = new Worker(workerFile, { workerData: increment });
+
+    worker.on("message", (msg) => console.log(msg));
+    increment++;
+  });
 };
 
 await performCalculations();
