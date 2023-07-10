387cd9441e7e58bf9cd8803a522cbc6513d65483
Nikita Staselovich <nikitastaselovich@gmail.com>
2023-07-10 14:18:20 +0300

    ADD: add solution to zlib tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import path from "path";
+import { createGzip } from "zlib";
+import { createReadStream, createWriteStream } from "fs";
+
 const compress = async () => {
-    // Write your code here 
+  const fileToCompress = path.resolve(
+    process.cwd() + "/src/zip/files/fileToCompress.txt"
+  );
+  const compressedFile = path.resolve(
+    process.cwd() + "/src/zip/files/archive.gz"
+  );
+
+  createReadStream(fileToCompress)
+    .pipe(createGzip())
+    .pipe(createWriteStream(compressedFile))
+    .on("finish", () => console.log("File has been compressed."));
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+import path from "path";
+import { createUnzip } from "zlib";
+import { createReadStream, createWriteStream } from "fs";
+
 const decompress = async () => {
-    // Write your code here 
+  const fileToDecompress = path.resolve(
+    process.cwd() + "/src/zip/files/archive.gz"
+  );
+
+  const decompressedFile = path.resolve(
+    process.cwd() + "/src/zip/files/fileToCompress.txt"
+  );
+
+  createReadStream(fileToDecompress)
+    .pipe(createUnzip())
+    .pipe(createWriteStream(decompressedFile));
 };
 
 await decompress();
