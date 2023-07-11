2c74099776ad31e62d754638ea58a975007769d3
Nikita Staselovich <nikitastaselovich@gmail.com>
2023-07-11 10:40:11 +0300

    ADD: add solution to cp tasks

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,10 @@
+import { fork } from "child_process";
+import path from "path";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const fileToFork = path.resolve(process.cwd() + "/src/cp/files/script.js");
+  const child = fork(fileToFork, args);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([2, 3, 4, 5, 6, 4393939, 43, "CLOSE"]);
